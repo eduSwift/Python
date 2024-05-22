3379d340a331ffcd9969403706862e741f825591
eduSwift <eduSwift@yahoo.com>
2024-05-22 20:28:23 +0000

    BMI measure fix

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -1,5 +1,5 @@
-height = int(input("Give me your height: "))
-weight = int(input("Give me your weight: "))
+height = int(input("Give me your height (in cm): "))
+weight = int(input("Give me your weight (in kg): "))
 bmi = weight / (height / 100.0) / (height / 100.0)
 print( f"Your BMI is: {bmi}")
 
