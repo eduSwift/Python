660dfdc98fa56cc0ce35936f41a476a64c5a98f5
eduSwift <eduSwift@yahoo.com>
2024-05-28 19:13:50 +0200

    newfile

--- test.py
+++
@@ -1,6 +0,0 @@
-age = input()
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-your_life_in_weeks = 90 * 52 - age * 52
-your_life_in_weeks_int = (int(your_life_in_weeks))
-print(f"You have {your_life_in_weeks_int} weeks left")
