120908b409b0fab764113dde592f9d85f74771a9
eduSwift <eduSwift@yahoo.com>
2024-06-01 22:32:31 +0200

    init

--- age.py
+++
@@ -1,6 +0,0 @@
-age = input("Give me your age: ")
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-your_life_in_weeks = 90 * 52 - int(age) * 52
-your_life_in_weeks_int = (int(your_life_in_weeks))
-print(f"You have {your_life_in_weeks_int} weeks left")
