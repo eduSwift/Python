90ad4770459a33b0bf61faf7726fc098c072886e
eduSwift <eduSwift@yahoo.com>
2024-06-03 07:38:31 +0200

    bug fix

--- lifeInWeeks.py
+++ lifeInWeeks.py
@@ -1,6 +1,6 @@
-age = input()
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-your_life_in_weeks = 90 * 52 - age * 52
-your_life_in_weeks_int = (int(your_life_in_weeks))
-print(f"You have {your_life_in_weeks_int} weeks left")
+print("Welcome to your life in weeks!")
+age = input("Enter your current age")
+years = 90 - int(age)
+weeks = years * 52
+print(f"You have {weeks} weeks left. ")
+
